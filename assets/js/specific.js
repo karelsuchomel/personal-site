9dbbeb2fa879d9983a70f688bacc1b7893f8912a
Karel Suchomel <karelsuchomel@windowslive.com>
2018-05-01 11:21:01 +0200

    Added function assembleAddress

--- assets/js/specific.js
+++ assets/js/specific.js
@@ -1,21 +1,34 @@
 // assemble e-mail address!
 // trigger: assemble-address-trigger
 // container: address-container
+let fh = "karelsuchomel@";
+let sh = "windowslive.com";
+let emailAddress = fh + sh;
 
-let emailAddress = "karelsuchomel@windowslive.com";
-
+// creates two dimensional array with shuffeled address in first
+// and original position of pices in second
 function shuffleArray(array) 
 {
 	let counter = array.length;
+	let origPosArr = [];
+	for (var i = 0; i < array.length; i++) {
+		origPosArr.push(i);
+	}
 
 	while (counter > 0) {
 		let index = Math.floor(Math.random() * counter--);
+		// address
 		let temp = array[counter];
 		array[counter] = array[index];
 		array[index] = temp;
+		// original positions
+		let posTemp = origPosArr[counter];
+		origPosArr[counter] = origPosArr[index];
+		origPosArr[index] = posTemp;
 	}
-	return array;
-}
+	const returnArray = [array, origPosArr];
+	return returnArray;
+};
 
 function initiateChaosUponAddress( address, containerID, triggerID ) 
 {
@@ -37,17 +50,30 @@ function initiateChaosUponAddress( address, containerID, triggerID )
 	console.log(shuffleArray(piecesArr));
 
 	let htmlContent = "";
-	for (var i = 0; i < piecesArr.length; i++) {
+	for (var i = 0; i < piecesArr[1].length; i++) {
 		let rndPosX = Math.floor(Math.random() * Math.floor(50));
 		let rndPosY = Math.floor(Math.random() * Math.floor(50));
 		let rndRot = Math.floor(Math.random() * Math.floor(359));
 		let transformStyle = "transform: translate(" + rndPosX + "px, " + rndPosY + "px) rotate(" + rndRot + "deg);";
-		htmlContent += '<span style="' + transformStyle + '">' + piecesArr[i] + '</span>';
+		htmlContent += '<span style="' + transformStyle + '">' + piecesArr[1][i] + '</span>';
 	}
 
 	console.log(htmlContent);
 
 	contEl.innerHTML = htmlContent;
+	trigEl.addEventListener('click', assembleAddress(piecesArr, contEl) );
 };
 
-initiateChaosUponAddress(emailAddress, "address-container");
+function assembleAddress(piecesArr, contEl)
+{
+	let spanArr = contEl.getElementsByTagName("span");
+	for (var i = 0; i < spanArr.length; i++) {
+		spanArr[i].style = "";
+	}
+
+
+	// reset the transform styles
+	// bublesort, after each swap, render the html
+};
+
+initiateChaosUponAddress(emailAddress, "address-container", "assemble-address-trigger");
